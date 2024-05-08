dbc5f68b4223008cd17850bdeef373de7bd7ddbc
Edward R <66365396+diddlemeister@users.noreply.github.com>
2024-05-08 23:23:27 +1000

    Created basic structure of ELIAS'

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import Message from './Message';
+
+function App() {
+  return <div style={{backgroundColor: "#333333", height: '100vh'}}><Message /></div>;
+}
+
+export default App;
---
+++ src/Message.tsx
@@ -0,0 +1,11 @@
+function Message() {
+  const name = "ELIAS";
+  const style = {
+    color: "white",
+    padding: "20px",
+  };
+
+  return <h1 style={style}>Hello {name}</h1>;
+}
+
+export default Message;
